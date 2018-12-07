f1bd8a63fc697f843d4c22463c29dbb6de4ca2b7
Philipp Haefelfinger <philipp@haefelfinger.ch>
2018-12-07 21:22:43 +0100

    removed privacy view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,11 +15,6 @@ namespace Tv7Playlist.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
